e7095f2f8ccdf96788bba187771f8fce0e59722c
Nikita Primak <blamelessraven@yandex.ru>
2023-09-04 17:01:55 +0300

    try fix

--- mymath.go
+++ mymath.go
@@ -6,28 +6,8 @@ func Sqrt(x float64) float64 {
 	return math.Sqrt(x)
 }
 
-func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
-}
-
-func Sin(x float64) float64 {
-	return math.Sin(x)
-}
-
-func Cos(x float64) float64 {
-	return math.Cos(x)
-}
-
-func Tan(x float64) float64 {
-	return math.Tan(x)
-}
-
 func Max(x, y float64) float64 {
 	return math.Max(x, y)
-}
-
-func Min(x, y float64) float64 {
-	return math.Min(x, y)
 }
 
 func Abs(x float64) float64 {
@@ -36,14 +16,4 @@ func Abs(x float64) float64 {
 
 func Yn(x int, y float64) float64 {
 	return math.Yn(x, y)
-}
-
-func Acos(x float64) float64 {
-	return math.Acos(x)
-}
-func Acosh(x float64) float64 {
-	return math.Acosh(x)
-}
-func Asin(x float64) float64 {
-	return math.Asin(x)
 }
